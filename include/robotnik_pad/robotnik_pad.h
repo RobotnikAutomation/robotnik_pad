df290a78c737ef3ec679d3455ab97a68db199742
David <dredo@robotnik.es>
2020-01-08 13:08:40 +0100

    Implemented standbyState, rosSetup and rosReadParams functions

--- include/robotnik_pad/robotnik_pad.h
+++ include/robotnik_pad/robotnik_pad.h
@@ -2,6 +2,8 @@
 #ifndef _ROBOTNIK_PAD_
 #define _ROBOTNIK_PAD_
 
+#include <ackermann_msgs/AckermannDrive.h>
+#include <geometry_msgs/Twist.h>
 #include <math.h>
 #include <ros/console.h>
 #include <ros/ros.h>
@@ -11,11 +13,25 @@
 #include <rcomponent/rcomponent.h>
 #include <robotnik_pad/button.h>
 
+#define DEFAULT_NUM_OF_BUTTONS 16
+#define DEFAULT_NUM_OF_AXES 8
+#define DEFAULT_AXIS_LINEAR_X 1
+#define DEFAULT_AXIS_LINEAR_Y 0
+#define DEFAULT_AXIS_ANGULAR 2
+#define DEFAULT_SCALE_LINEAR 1.5
+#define DEFAULT_SCALE_ANGULAR 1.5
 class RobotnikPad : public rcomponent::RComponent
 {
 public:
   RobotnikPad(ros::NodeHandle h);
   virtual ~RobotnikPad();
+
+  enum KinematicMode
+  {
+    Differential = 0,
+    Omnidirectional = 1,
+    Ackermann = 2
+  };
 
 protected:
   /* RComponent stuff */
@@ -41,6 +57,8 @@ protected:
 
   //! joy callback
   void joyCb(const sensor_msgs::Joy::ConstPtr& joy);
+  void fillTwistMsg(double linear_x, double linear_y, double angular_z);
+  void fillAckermannMsg(double linear_x, double angle);
 
 public:
 protected:
@@ -53,6 +71,25 @@ protected:
 
   // Subscribers
   ros::Subscriber joy_sub_;
+
+  // JOYSTICK
+  //! Current number of buttons of the joystick
+  int num_of_buttons_;
+  int num_of_axes_;
+  std::string pad_type_;
+  int axis_linear_x_, axis_linear_y_, axis_angular_z_;
+  double a_scale_, l_scale_;
+  std::string cmd_twist_topic_vel_, cmd_ackermann_topic_vel_;
+  int dead_man_button_, speed_up_button_, speed_down_button_;
+
+  // KINEMATIC MODE
+  int button_kinematic_mode_, kinematic_mode_;
+
+  //! Vector to save the axis values
+  std::vector<float> axes_;
+  //! Vector to save and control the axis values
+  std::vector<Button> buttons_;
+
 };
 
 #endif  // _ROBOTNIK_PAD_
--- src/robotnik_pad.cpp
+++ src/robotnik_pad.cpp
@@ -12,6 +12,20 @@ RobotnikPad::~RobotnikPad()
 
 int RobotnikPad::rosSetup()
 {
+  // Publishers
+  if (kinematic_mode_ == Ackermann)
+  {
+    ackermann_pub_ = pnh_.advertise<ackermann_msgs::AckermannDrive>(cmd_ackermann_topic_vel_, 10);
+  }
+  else
+  {
+    twist_pub_ = pnh_.advertise<geometry_msgs::Twist>(cmd_twist_topic_vel_, 10);
+  }
+
+  pad_status_pub_ = pnh_.advertise<robotnik_msgs::PadStatus>("pad_status", 10);
+
+  joy_sub_ = nh_.subscribe<sensor_msgs::Joy>("joy", 10, &RobotnikPad::joyCb, this);
+  addTopicsHealth(&joy_sub_, "joy", 5);
 }
 
 int RobotnikPad::rosShutdown()
@@ -21,6 +35,28 @@ int RobotnikPad::rosShutdown()
 
 void RobotnikPad::rosReadParams()
 {
+  bool required = true;
+  bool not_required = false;
+
+  // JOYSTICK PAD TYPE
+  readParam(pnh_, "num_of_buttons", num_of_buttons_, DEFAULT_NUM_OF_BUTTONS, required);
+  readParam(pnh_, "num_of_axes", num_of_axes_, DEFAULT_NUM_OF_AXES, required);
+  readParam(pnh_, "pad_type", pad_type_, "ps4", required);
+
+  readParam(pnh_, "axis_linear_x", axis_linear_x_, DEFAULT_AXIS_LINEAR_X, required);
+  readParam(pnh_, "axis_linear_y", axis_linear_y_, DEFAULT_AXIS_LINEAR_Y, required);
+  readParam(pnh_, "axis_angular_z", axis_angular_z_, DEFAULT_AXIS_ANGULAR, required);
+  readParam(pnh_, "scale_angular", a_scale_, DEFAULT_SCALE_ANGULAR, required);
+  readParam(pnh_, "scale_linear", l_scale_, DEFAULT_SCALE_LINEAR, required);
+  readParam(pnh_, "cmd_twist_topic_vel", cmd_twist_topic_vel_, cmd_twist_topic_vel_, required);
+  readParam(pnh_, "cmd_ackermann_topic_vel", cmd_ackermann_topic_vel_, cmd_ackermann_topic_vel_, required);
+  readParam(pnh_, "button_dead_man", dead_man_button_, dead_man_button_, required);
+  readParam(pnh_, "button_speed_up", speed_up_button_, speed_up_button_, required);
+  readParam(pnh_, "button_speed_down", speed_down_button_, speed_down_button_, required);
+
+  // KINEMATIC MODE
+  readParam(pnh_, "kinematic_mode", kinematic_mode_, kinematic_mode_, required);
+  readParam(pnh_, "button_kinematic_mode", button_kinematic_mode_, button_kinematic_mode_, required);
 }
 
 void RobotnikPad::rosPublish()
@@ -33,6 +69,14 @@ void RobotnikPad::initState()
 
 void RobotnikPad::standbyState()
 {
+  if (checkTopicsHealth() == false)
+  {
+    switchToState(robotnik_msgs::State::EMERGENCY_STATE);
+  }
+  else
+  {
+    switchToState(robotnik_msgs::State::READY_STATE);
+  }
 }
 
 void RobotnikPad::readyState()
@@ -48,5 +92,22 @@ void RobotnikPad::failureState()
 }
 
 void RobotnikPad::joyCb(const sensor_msgs::Joy::ConstPtr& joy)
+{
+  // Fill in the axes and buttons arrays
+  for (int i = 0; i < joy->axes.size(); i++)
+  {
+    axes_[i] = joy->axes[i];
+  }
+  for (int i = 0; i < joy->buttons.size(); i++)
+  {
+    buttons_[i].press(joy->buttons[i]);
+  }
+}
+
+void RobotnikPad::fillTwistMsg(double linear_x, double linear_y, double angular_z)
+{
+}
+
+void RobotnikPad::fillAckermannMsg(double linear_x, double angle)
 {
 }
