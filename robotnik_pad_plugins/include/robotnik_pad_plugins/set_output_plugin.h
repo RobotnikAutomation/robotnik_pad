f05af5e7767c1a00a5c19f9c0e6245dd2e311adb
Jose Antonio Mendez <jamendez@robotnik.es>
2024-12-03 16:04:51 +0100

    Complete set output plugin and add input condition to activate output

--- robotnik_pad_plugins/include/robotnik_pad_plugins/set_output_plugin.h
+++ robotnik_pad_plugins/include/robotnik_pad_plugins/set_output_plugin.h
@@ -7,6 +7,11 @@
 
 namespace pad_plugins
 {
+
+struct io {
+  int number;
+  bool value;
+};
 class PadPluginSetOutput : public GenericPadPlugin
 {
 public:
@@ -18,7 +23,11 @@ public:
 
 protected:
   bool checkFeedback(bool invert_values);
+  bool checkCondition();
   void ioCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg);
+  void feedbackIoCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg);
+  void conditionIoCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg);
+  void getIoValues(ros::NodeHandle& nh, const std::string& param_name, std::vector<io>& variable);
 
   double timeout_;
   int button_dead_man_;
@@ -27,17 +36,20 @@ protected:
   bool activate_, deactivate_;
   bool use_plc_;
   bool has_feedback_;
-  std::vector<int> outputs_;
-  std::vector<bool> output_values_;
-  std::vector<int> inputs_;
-  std::vector<bool> input_values_; 
+  bool has_condition_;
+  bool use_buttons_;
+  int button_set_output_;
+  int button_reset_output_;
+  std::vector<io> outputs_;
+  std::vector<io> inputs_;
+  std::vector<io> condition_inputs_;
   std::string base_hw_set_output_name_;
   std::string modbus_set_output_name_;
-  std::string base_hw_io_topic_name_;
-  std::string modbus_io_topic_name_;
+  std::string feedback_input_topic_name_;
+  std::string condition_input_topic_name_;
   ros::ServiceClient set_output_client_;
-  ros::Subscriber io_sub_;
-  robotnik_msgs::inputs_outputs io_;
+  ros::Subscriber io_sub_, feedback_io_sub_, condition_io_sub_;
+  robotnik_msgs::inputs_outputs io_, condition_io_;
   ros::Time init_timeout_;
 };
 }  // namespace pad_plugins
--- robotnik_pad_plugins/src/set_output_plugin.cpp
+++ robotnik_pad_plugins/src/set_output_plugin.cpp
@@ -22,52 +22,37 @@ void PadPluginSetOutput::initialize(const ros::NodeHandle& nh, const std::string
 
   readParam(pnh_, "config/deadman", button_dead_man_, button_dead_man_, required);
   readParam(pnh_, "config/axis_set_output", axis_set_output_, axis_set_output_, required);
+  readParam(pnh_, "config/button_set_output", button_set_output_, button_set_output_, required);
+  readParam(pnh_, "config/button_reset_output", button_reset_output_, button_reset_output_, required);
+  readParam(pnh_, "use_buttons", use_buttons_, false, not_required);
   readParam(pnh_, "base_hw_set_output_name", base_hw_set_output_name_, base_hw_set_output_name_, required);
   readParam(pnh_, "modbus_set_output_name", modbus_set_output_name_, modbus_set_output_name_, required);
-  readParam(pnh_, "use_plc", use_plc_, false, not_required);
+  readParam(pnh_, "use_plc", use_plc_, false, required);
   readParam(pnh_, "has_feedback", has_feedback_, false, not_required);
-  pnh_.getParam("outputs", outputs_);
-  pnh_.getParam("inputs", inputs_);
-  pnh_.getParam("output_values", output_values_);
-  pnh_.getParam("input_values", input_values_);
-  // readParam(pnh_, "outputs", outputs_, void_int_list, required);
-  // readParam(pnh_, "output_values", output_values_, void_bool_list, not_required);
-  // readParam(pnh_, "inputs", inputs_, void_int_list, not_required);
-  // readParam(pnh_, "input_values", input_values_, void_bool_list, not_required);
+  readParam(pnh_, "has_condition", has_condition_, false, not_required);
   timeout_ = 1;
   readParam(pnh_, "timeout", timeout_, timeout_, not_required);
+  if (use_plc_)
+    feedback_input_topic_name_ = "robotnik_modbus_io/inputs_outputs";
+  else
+    feedback_input_topic_name_ = "robotnik_base_hw/io";
+  condition_input_topic_name_ = feedback_input_topic_name_;
 
-  base_hw_io_topic_name_ = "";
-  readParam(pnh_, "base_hw_io_topic_name", base_hw_io_topic_name_, base_hw_io_topic_name_, not_required);
-  modbus_io_topic_name_ = "";
-  readParam(pnh_, "modbus_io_topic_name", modbus_io_topic_name_, modbus_io_topic_name_, not_required);
-  int n = outputs_.size();
-  int n_val = output_values_.size();
-  if (n != n_val)
-  {
-    output_values_.clear();
-    for (int i = 0; i < n; i++)
-      output_values_[i] = true;
-  }
+  readParam(pnh_, "feedback_input_topic_name", feedback_input_topic_name_, feedback_input_topic_name_, not_required);
+  readParam(pnh_, "condition_input_topic_name", condition_input_topic_name_, condition_input_topic_name_, not_required);
+
+  getIoValues(pnh_, "outputs", outputs_);
   if (has_feedback_)
   {
-    n = inputs_.size();
-    if (n == 0)
+    getIoValues(pnh_, "feedback_inputs", inputs_);
+    if (inputs_.size() == 0)
       has_feedback_ = false;
-    else if (base_hw_io_topic_name_ == "" && !use_plc_)
-      has_feedback_ = false;
-    else if (modbus_io_topic_name_ == "" && use_plc_)
-      has_feedback_ = false;
-    else
-    {
-      n_val = input_values_.size();
-      if (n != n_val)
-      {
-        input_values_.clear();
-        for (int j = 0; j < n; j++)
-          input_values_[j] = true;
-      }
-    }
+  }
+  if (has_condition_)
+  {
+    getIoValues(pnh_, "condition_inputs", condition_inputs_);
+    if (condition_inputs_.size() == 0)
+      has_condition_ = false;
   }
 
   // Service client
@@ -75,35 +60,88 @@ void PadPluginSetOutput::initialize(const ros::NodeHandle& nh, const std::string
   set_output_client_ = nh_.serviceClient<robotnik_msgs::set_digital_output>(service_name);
 
   //Subscriber
-  if (has_feedback_)
+  if (has_feedback_ || has_condition_)
   {
-    std::string topic_name = use_plc_ ? modbus_io_topic_name_ : base_hw_io_topic_name_;
-    io_sub_ = nh_.subscribe<robotnik_msgs::inputs_outputs>(topic_name, 1, &PadPluginSetOutput::ioCb, this);
+    if (feedback_input_topic_name_ == condition_input_topic_name_ && has_feedback_ && has_condition_)
+    {
+      io_sub_ = nh_.subscribe<robotnik_msgs::inputs_outputs>(feedback_input_topic_name_, 1, &PadPluginSetOutput::ioCb, this);
+    }
+    else
+    {
+      if (has_feedback_)
+      {
+        feedback_io_sub_ = nh_.subscribe<robotnik_msgs::inputs_outputs>(feedback_input_topic_name_, 1, &PadPluginSetOutput::feedbackIoCb, this);
+      }
+      if (has_condition_)
+      {
+        condition_io_sub_ = nh_.subscribe<robotnik_msgs::inputs_outputs>(condition_input_topic_name_, 1, &PadPluginSetOutput::conditionIoCb, this);
+      }
+    }
+  }
+}
+
+void PadPluginSetOutput::getIoValues(ros::NodeHandle& nh, const std::string& param_name, std::vector<io>& variable)
+{
+  XmlRpc::XmlRpcValue in_out;
+  nh.getParam(param_name, in_out);
+  int i;
+  io aux_in_out;
+  if (in_out.getType() == XmlRpc::XmlRpcValue::TypeArray)
+  {
+    for (i = 0; i < in_out.size(); i++)
+    {
+      aux_in_out.number = in_out[i]["number"];
+      aux_in_out.value = in_out[i]["value"];
+      variable.push_back(aux_in_out);
+    }
+  }
+  else if (in_out.getType() == XmlRpc::XmlRpcValue::TypeStruct)
+  {
+    aux_in_out.number = in_out["number"];
+    aux_in_out.value = in_out["value"];
+    variable.push_back(aux_in_out);
+  }
+  else
+  {
+    ROS_ERROR("Parameter %s does not contain a list or a struct with int 'number' and bool 'value'.", param_name.c_str());
   }
 }
 
 void PadPluginSetOutput::ioCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg)
 {
   io_ = *msg;
+  condition_io_ = *msg;
+}
+
+void PadPluginSetOutput::feedbackIoCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg)
+{
+  io_ = *msg;
+}
+
+void PadPluginSetOutput::conditionIoCb(const robotnik_msgs::inputs_outputs::ConstPtr& msg)
+{
+  condition_io_ = *msg;
 }
 
 void PadPluginSetOutput::execute(const std::vector<Button>& buttons, std::vector<float>& axes)
 {
   if (buttons[button_dead_man_].isPressed())
   {
-    if (axes[axis_set_output_] > 0.95)
+    if (((axes[axis_set_output_] > 0.95 && !use_buttons_) || (buttons[button_set_output_].isReleased() && use_buttons_)) && checkCondition())
     {
       if (!output_sent_)
       {
+        ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Setting outputs...");
         output_sent_ = true;
         activate_ = true;
         init_timeout_ = ros::Time::now();
       }
     }
-    if (axes[axis_set_output_] < -0.95)
+    if (((axes[axis_set_output_] < -0.95  && !use_buttons_) || (buttons[button_reset_output_].isReleased() && use_buttons_)) && checkCondition())
     {
       if (!output_sent_)
       {
+        ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Resetting outputs...");
         output_sent_ = true;
         deactivate_ = true;
         init_timeout_ = ros::Time::now();
@@ -119,38 +157,50 @@ void PadPluginSetOutput::execute(const std::vector<Button>& buttons, std::vector
     robotnik_msgs::set_digital_output set_output_srv;
     if (activate_)
     {
-      ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Setting outputs...");
       for (int i = 0; i < outputs_.size(); i++)
       {
-        set_output_srv.request.output = outputs_[i];
-        set_output_srv.request.value = output_values_[i];
-      if (set_output_client_.call(set_output_srv) != true || set_output_srv.response.ret != true)
-      {
-        ROS_ERROR_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Error setting outputs");
-      }
-      }
-      if (checkFeedback(false) || ros::Time::now() - init_timeout_ > ros::Duration(timeout_))
-      {
-        output_sent_ = false;
-        activate_ = false;
-      }
-    }
-    else if (deactivate_)
-    {
-      ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Setting outputs...");
-      for (int i = 0; i < outputs_.size(); i++)
-      {
-        set_output_srv.request.output = outputs_[i];
-        set_output_srv.request.value = !output_values_[i];
+        set_output_srv.request.output = outputs_[i].number;
+        set_output_srv.request.value = outputs_[i].value;
         if (set_output_client_.call(set_output_srv) != true || set_output_srv.response.ret != true)
         {
           ROS_ERROR_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Error setting outputs");
         }
       }
-      if (checkFeedback(true) || ros::Time::now() - init_timeout_ > ros::Duration(timeout_))
+      if (checkFeedback(false))
+      {
+        output_sent_ = false;
+        activate_ = false;
+        ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Outputs correctly set");
+      }
+      else if (ros::Time::now() - init_timeout_ > ros::Duration(timeout_))
+      {
+        output_sent_ = false;
+        activate_ = false;
+        ROS_ERROR_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Error setting outputs after %.2f seconds", timeout_);
+      }
+    }
+    else if (deactivate_)
+    {
+      for (int i = 0; i < outputs_.size(); i++)
+      {
+        set_output_srv.request.output = outputs_[i].number;
+        set_output_srv.request.value = !outputs_[i].value;
+        if (set_output_client_.call(set_output_srv) != true || set_output_srv.response.ret != true)
+        {
+          ROS_ERROR_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Error resetting outputs");
+        }
+      }
+      if (checkFeedback(true))
       {
         output_sent_ = false;
         deactivate_ = false;
+        ROS_INFO_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Outputs correctly reset");
+      }
+      else if (ros::Time::now() - init_timeout_ > ros::Duration(timeout_))
+      {
+        output_sent_ = false;
+        deactivate_ = false;
+        ROS_ERROR_NAMED("PadPluginSetOutput", "PadPluginSetOutput::execute: Error resetting outputs after %.2f seconds", timeout_);
       }
     }
   }
@@ -165,7 +215,20 @@ bool PadPluginSetOutput::checkFeedback(bool invert_values)
 
   for (int i = 0; i < inputs_.size(); i++)
   {
-    success &= io_.digital_inputs[inputs_[i] - 1] == (input_values_[i] ^ invert_values);
+    success &= io_.digital_inputs[inputs_[i].number - 1] == (inputs_[i].value ^ invert_values);
+  }
+  return success;
+}
+
+bool PadPluginSetOutput::checkCondition()
+{
+  if (!has_condition_)
+    return true;
+
+  bool success = true;
+  for (int i = 0; i < condition_inputs_.size(); i++)
+  {
+    success &= (condition_io_.digital_inputs[condition_inputs_[i].number - 1] == condition_inputs_[i].value);
   }
   return success;
 }
