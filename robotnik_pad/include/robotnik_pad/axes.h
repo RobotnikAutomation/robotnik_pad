a48bfe739b7fc8edf348d07a3a371e494fd5b0e7
Jose Antonio Mendez <jamendez@robotnik.es>
2025-04-02 13:28:27 +0200

    robotnik_pad/axes.h: Reduce operations in press function, rename variables and edit isReleased functions

--- robotnik_pad/include/robotnik_pad/axes.h
+++ robotnik_pad/include/robotnik_pad/axes.h
@@ -6,60 +6,53 @@
 //! Class to save the state of the axes
 class Axes
 {
-    int is_pressed_pos{0};
-    int is_pressed_neg{0};
-    bool is_released_pos{false};
-    bool is_released_neg{false};
-    float axis_value{0.0};
+    bool is_released_pos_{false};
+    bool is_released_neg_{false};
+    float axis_value_{0.0};
 
 public:
     Axes(){}
 
     void press(float value)
     {
-        bool isValuePositive = (value > 0);
-        bool isValueNegative = (value < 0);
-
-        is_released_pos = (is_pressed_pos && !isValuePositive);
-        is_released_neg = (is_pressed_neg && !isValueNegative);
-
-        is_pressed_pos = isValuePositive;
-        is_pressed_neg = isValueNegative;
+        is_released_pos_ = (axis_value_ > 0 && !(value > 0));
+        is_released_neg_ = (axis_value_ < 0 && !(value < 0));
 		
-        axis_value = value;
+        axis_value_ = value;
     }
 
     int isPressedPos(const double threshold = 0) const
     {
-        return is_pressed_pos && axis_value > abs(threshold);
+
+        return axis_value_ > abs(threshold);
     }
 
     int isPressedNeg(const double threshold = 0) const
     {
-        return is_pressed_neg && axis_value < -abs(threshold);
+        return axis_value_ < -abs(threshold);
     }
 
-    bool isReleasedPos(const double threshold = 0) const
+    bool isReleasedPos() const
     {
-        return is_released_pos && axis_value <= abs(threshold);
+        return is_released_pos_;
     }
 
-    bool isReleasedNeg(const double threshold = 0) const
+    bool isReleasedNeg() const
     {
-        return is_released_neg && axis_value >= -abs(threshold);
+        return is_released_neg_;
     }
 
     void resetReleased()
     {
-        if (is_released_pos)
-            is_released_pos = false;
-        if (is_released_neg)
-            is_released_neg = false;
+        if (is_released_pos_)
+            is_released_pos_ = false;
+        if (is_released_neg_)
+            is_released_neg_ = false;
     }
 
     float getValue() const
     {
-        return axis_value;
+        return axis_value_;
     }
 };
 
